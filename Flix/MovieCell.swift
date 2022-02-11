67fb8d53da83141d4b8d3f88d98080d0b57fcd84
Brian Velecela <70337936+brianV05@users.noreply.github.com>
2022-02-11 14:08:12 -0500

    files were added

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Brian Velecela on 2/10/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    //add all outlet for the Custom View Table Cell
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Brian Velecela on 2/7/22.
+//
+
+import UIKit
+import AlamofireImage //the special library
+
+//step 1: ADD UITableViewDataSource and UITableViewDelegate
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //stores the list of movies from the API's result section
+    //var movie is an array of dictionaries
+    var movies = [[String:Any]]() // (), means the creation of something
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //step 3
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        // Do any additional setup after loading the view.
+        print("Hello")
+        
+        //API
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                 
+                 //"hey movies, i want you to look at in that data dictionary and i want you to get out "result""
+                 // Casting -
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 
+                 //we need this because the function below, are not called continously, so we need it to update
+                 //reloadlaod - it has the call of these functions again
+                 self.tableView.reloadData()
+                 
+                 //this will print datadicationary in the terminal
+                 print(dataDictionary)
+                 
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+             }
+        }
+        task.resume()
+    }
+    
+    
+    //from step 2
+    //asking for the number of rows
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section: Int) -> Int{
+        //the amount of rows
+        return movies.count
+    }
+    
+    //"for this partically row, give me the cell"
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //create a new cell
+        /*let cell = UITableViewCell()*/
+        
+        
+        /*.dequeueReusableCell - at any point of time, you may have 100+ rows of movies. So that many cells will take a lot of memory
+        DQ does is recycles cells*/
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        //accessing the movies
+        let movie = movies[indexPath.row]
+        
+        //(getting the title)"hey what is the title of that movie"
+        //we need to access it by some key. Inside the API, you see the word "title", given the title of the movie
+        let title = movie["title"] as! String
+        
+        //getting the description(synopsis) of the movie
+        let synopsis = movie["overview"] as! String
+        
+        //getting the picture of the movie
+        let baseUrl = "https://image.tmdb.org/t/p/w185" //setting the size of the pic
+        let posterPath = movie["poster_path"] as! String // retrieving data "poster_path" from API
+        let posterUrl = URL(string: baseUrl + posterPath)! //combine the size and poster_path
+        
+        /*cell.textLabel?.text = title*/
+        
+        // ? - swift optionals
+        // display the title
+        cell.titleLabel.text = title
+        //display the synopsis(description)
+        cell.synopsisLabel.text = synopsis //make sure to configure the line to 0, for multiple lines
+        //display the picture of the movie
+        cell.posterView.af.setImage(withURL: posterUrl) //cell.posterView.af_setImage ->old syntax
+        return cell
+    }
+}
+ 
