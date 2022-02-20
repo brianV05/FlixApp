0a30f42f10ee570a52c72b9949afb934aee48783
Brian Velecela <70337936+brianV05@users.noreply.github.com>
2022-02-20 00:07:18 -0500

    added trailer view controller

---
+++ Flix/TrailerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TrailerViewController.swift
+//  Flix
+//
+//  Created by Brian Velecela on 2/19/22.
+//
+
+import UIKit
+import WebKit
+
+class TrailerViewController: UIViewController, WKUIDelegate {
+
+    @IBOutlet weak var TrailerView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+
+}
